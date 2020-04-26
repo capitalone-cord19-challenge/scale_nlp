8d6bee261575054042c961bb969ef9ce983b61f0
TopThisHat <ralph.lozano88@gmail.com>
2020-04-26 19:10:22 -0400

    Update runner.py

--- runner.py
+++ runner.py
@@ -5,10 +5,10 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
 
     parser.add_argument("--rank_path", default='models/bert_ranking', type=str)
-    parser.add_argument("--rank_batchsize", default=8, type=int)
+    parser.add_argument("--ranking_batchsize", default=8, type=int)
     parser.add_argument("--index", default="../index", type=str)
     parser.add_argument("--number_docs", default=10, type=int)
-    parser.add_argument("--max_seq", default=384, type=int)
+    parser.add_argument("--max_sequence", default=384, type=int)
     parser.add_argument("--max_query", default=64, type=int)
     parser.add_argument("--stride", default=256, type=int)
     parser.add_argument("--lower", default=True)
